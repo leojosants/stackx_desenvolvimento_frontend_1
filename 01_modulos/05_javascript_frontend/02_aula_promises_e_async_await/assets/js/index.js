549ece8b93e2ffacbb559e3a8e822848a80da421
Leonardo Santos <leonardojosantos@gmail.com>
2024-03-28 13:13:46 -0300

    Aula -Promises & Async Await

---
+++ 01_modulos/05_javascript_frontend/02_aula_promises_e_async_await/assets/js/index.js
@@ -0,0 +1,15 @@
+const newPromise = new Promise((resolve, reject) => {
+    const password = '12345';
+
+    if (password === '12345') {
+        resolve('Senha correta');
+    }
+    else {
+        reject('Senha incorreta');
+    }
+});
+
+newPromise
+    .then((data) => data.toUpperCase())
+    .then((processedData) => processedData.repeat(3))
+    .then((processedDataRepeat) => console.log(processedDataRepeat));
