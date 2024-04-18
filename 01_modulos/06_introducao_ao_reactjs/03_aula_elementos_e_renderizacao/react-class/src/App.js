a0f2a2352c7131b14a01e77493ad16b03bc15d4a
Leonardo Santos <leonardojosantos@gmail.com>
2024-04-18 12:32:43 -0300

    Aula - Elementos e renderização

---
+++ 01_modulos/06_introducao_ao_reactjs/03_aula_elementos_e_renderizacao/react-class/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  const element = <p>Primeiro elemento</p>;
+  return (
+    <div>
+      <h1>Olá, JSX</h1>
+      <p>Meu primeiro JSX</p>
+      {element}
+    </div>
+  );
+}
+
+export default App;
